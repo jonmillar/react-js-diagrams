cbc75f8b490e1fb5b2c1e4918177774dbdd96c27
umarashfaq <umar.mughal2@gmail.com>
2017-05-24 22:58:27 +0500

    Adds linkType parameter to LinkModel constructor to make it more consistent with rest of the API (like NodeModel)

--- demos/demo5/components/links/white/WhiteLinkModel.js
+++ demos/demo5/components/links/white/WhiteLinkModel.js
@@ -3,6 +3,5 @@ import { LinkModel } from '../../../../../src/main';
 export class WhiteLinkModel extends LinkModel {
     constructor() {
         super('white');
-        this.linkType = 'white';
     }
 }
