66725f6e77a72de0bb52fa1d37df5a25ba90adc9
umar87 <umar.ashfaq@emumba.com>
2017-05-24 16:19:47 +0500

    Cloned demo4 as demo5

---
+++ demos/demo5/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { Demo5 } from './demo5';
+
+window.onload = () => {
+  const rootEl = document.getElementById('root');
+  const render = Component => {
+    ReactDOM.render(
+      <Provider store={store}>
+        <AppContainer>
+          <Component />
+        </AppContainer>
+      </Provider>,
+      rootEl
+    );
+  };
+
+  render(Demo5);
+  if (module.hot) module.hot.accept('./demo5', () => render(Demo5));
+};
